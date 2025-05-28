10958214884393306c80dc3da16112e391eb31b5
Trần Minh Trí <tritmse173635@fpt.edu.vn>
2025-05-28 18:58:44 +0700

    setup Template BE

---
+++ EKE_Backend/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBTSS.Repository.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {      
+        Task<int> CompleteAsync();
+    }
+}
---
+++ EKE_Backend/Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace IBTSS.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
